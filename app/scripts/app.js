71faee2190741de7d502c16b5285540147944d35
Gilles Piou <gilles.piou@gmail.com>
2014-08-01 15:30:07 +0200

    Implementing input suggestion

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,19 @@
 var app = angular
   .module('Currency',['ngAnimate', 'ngResource'])
-  .controller('currencyCtrl', ['$scope', function($scope) {
+  .controller('currencyCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.currencies = [{ toCurrency: "EUR" }, { toCurrency: "USD" }, { toCurrency: "XBT" }];
     $scope.from = {
       value: 10,
       currency: 'USD'
     };
 
-    $scope.addCurrency = function() {
-      $scope.currencies.push({});
+    $http.get('https://gist.githubusercontent.com/pioug/aa0b42824d91a0b916af/raw/61fac7996969bcbaf22aac1a1c6a5eaf22ea835b/currencies.json')
+      .success(function(data) {
+        $scope.currenciesList = data;
+      });
+
+    $scope.addCurrency = function(currency) {
+      $scope.currencies.push({ toCurrency: currency });
     };
 
     $scope.$watch('from', function() {
