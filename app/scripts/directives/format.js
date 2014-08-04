7bc8fdeac4caf53dc4ee852c844864ed2b09e13e
Gilles Piou <gilles.piou@gmail.com>
2014-08-04 16:04:22 +0200

    Format value

---
+++ app/scripts/directives/format.js
@@ -0,0 +1,21 @@
+app.directive('format', ['$filter', function($filter) {
+  return {
+    require:'ngModel',
+    link: function(scope, element, attrs, controller) {
+
+      element.bind('blur', function() {
+        this.value = $filter('currency')(this.value, '');
+      });
+
+      controller.$formatters.push(function(value) {
+        return $filter('currency')(value, '');
+      });
+
+      controller.$parsers.push(function(value) {
+        var val = ('' + value).replace(/,/g, '');
+        return +val;
+      });
+
+    }
+  }
+}]);
